2e05796e1d74c9f936a79b987afc21d6dfd51091
alexlinehan17-lab <alexlinehan17@gmail.com>
2026-09-25 08:27:23 +0100

    Publish licensed Kobra component gallery

--- utils/pwaNavigation.ts
+++ utils/pwaNavigation.ts
@@ -1,6 +1,7 @@
 /** Workbox matches pathname + search, so standalone documents must also be
  * excluded when they carry query parameters (including embedded previews). */
 export const standaloneDocumentRoutes = [
+  /^\/components(?:\/|\?|$)/,
   /^\/(?:privacy|terms)(?:\.html)?(?:\?|$)/,
   /^\/landing(?:-dev\.html|-demo\.html)?(?:\?|$)/,
   /^\/certle(?:\.html|\/)?(?:\?|$)/,
